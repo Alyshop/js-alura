abbea1e4a81ee816633109b6d520e82f5dd47e42
Alyshopp <alyssondiego18@gmail.com>
2023-11-29 11:08:41 -0300

    colocando comentarios na func

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// document.querySelector é para selecionar o documento HTML , inner = dentro 
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Hora do Desafio'; 
 
@@ -9,17 +10,19 @@ function exibirMensagemNoConsole() {
     console.log('O botão foi clicado!')
 }
 
+//função para exibir alerta quando o botão alerta for clicado
 function verificarAlerta(){
     alert('Eu amo JS');
 }
 
 
-
+//função que  pede ao usuario e exibi na tela
 function verificarPrompt(){
   let cidade = prompt('Fale o nome de uma cidade do Brasil?');
    alert(`Estive em ${cidade} e lembrei de voce`);
 }
 
+// função que verifica a soma de dois numeros
 function verificarSoma(){
 
     let primeiroNumero = parseInt(prompt('Digite o primeiro numeros'));
